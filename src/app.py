0ff8874eecb279d3c5f54e319cf80dbe767f42ed
Simonas Bazinys <sbazinys@gmail.com>
2021-02-28 22:37:17 +0200

    new images added

--- src/app.py
+++ src/app.py
@@ -2,13 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "<img src='https://i.gifer.com/1ws.gif'>"
+    return "<img src='https://i.gifer.com/1ws.gif'><img src='https://beingabitchblog.files.wordpress.com/2017/11/you-can-do-it-gif-4.gif'><img src='https://media1.giphy.com/media/3oFzlUxAjIc3JvQvJK/giphy.gif'><img src='https://media4.giphy.com/media/82wOb7Qg9KuUvtdN3v/giphy.gif'>"
 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
 
 
