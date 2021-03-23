2b1d0498348a3fe6d71a9eb700f450d0773824cf
simonas-bazinys <58137233+simonas-bazinys@users.noreply.github.com>
2021-03-23 15:11:06 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<img src='https://i.gifer.com/1ws.gif'><img src='https://beingabitchblog.files.wordpress.com/2017/11/you-can-do-it-gif-4.gif'><img src='https://media1.giphy.com/media/3oFzlUxAjIc3JvQvJK/giphy.gif'><img src='https://media4.giphy.com/media/82wOb7Qg9KuUvtdN3v/giphy.gif'>"
+    return "<img src='https://i.gifer.com/1ws.gif'><img src='https://beingabitchblog.files.wordpress.com/2017/11/you-can-do-it-gif-4.gif'><img src='https://media1.giphy.com/media/3oFzlUxAjIc3JvQvJK/giphy.gif'><img src='https://media4.giphy.com/media/82wOb7Qg9KuUvtdN3v/giphy.gif'> "
 
 
 
