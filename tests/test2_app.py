0f07aeb298eceddfbdea31ddf447076b20f7acb3
simonas-bazinys <58137233+simonas-bazinys@users.noreply.github.com>
2021-04-02 13:52:42 +0300

    Update test2_app.py

--- tests/test2_app.py
+++ tests/test2_app.py
@@ -2,6 +2,6 @@
 from app import index
 
 def test2_index():
-    assert index() == "<img src='https://i.gifer.com/1ws.gif'><img src='https://beingabitchblog.files.wordpress.com/2017/11/you-can-do-it-gif-4.gif'><img src='https://media1.giphy.com/media/3oFzlUxAjIc3JvQvJK/giphy.gif'><img src='https://media4.giphy.com/media/82wOb7Qg9KuUvtdN3v/giphy.gif'>"
+    assert index() == "<img src='https://i.gifer.com/1ws.gif'><img src='https://beingabitchblog.files.wordpress.com/2017/11/you-can-do-it-gif-4.gif'><img src='https://media1.giphy.com/media/3oFzlUxAjIc3JvQvJK/giphy.gif'><img src='https://media4.giphy.com/media/82wOb7Qg9KuUvtdN3v/giphy.gif'> "
 
 
